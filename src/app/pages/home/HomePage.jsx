75d4ae42ea598a779ede4dc1350aa002a373702b
somethingofscientist <1shraddhanand@gmail.com>
2024-08-26 11:40:47 +0530

    fix some styles in mobile view and tab view

--- src/app/pages/home/HomePage.jsx
+++ src/app/pages/home/HomePage.jsx
@@ -4,14 +4,15 @@ import About from "../../component/about/About";
 import Projects from "../../component/projects/Project";
 import Contact from "../../component/contact-me/Contact";
 import Header from '../../component/Header/Header';
+import styles from './Homepage.module.css';
 
 export default function HomePage() {
   return (
-    <>
+    <div className={styles.container}>
       <Hero />
       <Projects />
       <About />
       <Contact />
-    </>
+    </div>
   )
 }
